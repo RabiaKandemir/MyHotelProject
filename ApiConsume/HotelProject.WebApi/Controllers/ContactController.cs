baf8f9b472bd9b9ddfcdc5f4bb0f90ec90a84520
RabiaKandemir <119038074+RabiaKandemir@users.noreply.github.com>
2025-05-11 23:00:49 +0300

    Message statistic

--- ApiConsume/HotelProject.WebApi/Controllers/ContactController.cs
+++ ApiConsume/HotelProject.WebApi/Controllers/ContactController.cs
@@ -47,11 +47,11 @@ namespace HotelProject.WebApi.Controllers
             var values = _contactService.TGetById(id);
             return Ok(values);
         }
-        //[HttpGet("{id}")]
-        //public IActionResult GetSendMessage(int id)
-        //{
-        //    var values = _contactService.TGetById(id);
-        //    return Ok(values);
-        //}
+        [HttpGet("GetContactCount")]
+        public IActionResult GetContactCount()
+        {
+            var values = _contactService.TGetContactCount();
+            return Ok(values);
+        }
     }
 }
--- Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -19,12 +19,19 @@ namespace HotelProject.WebUI.Controllers
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("http://localhost:5130/api/Contact");
+            var responseMessage2 = await client.GetAsync("http://localhost:5130/api/Contact/GetContactCount");
+            var responseMessage3 = await client.GetAsync("http://localhost:5130/api/SendMessage/GetSendMessageCount");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<InboxContactDto>>(jsonData);
+                var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+                ViewBag.contactCount = jsonData2;
+                var jsonData3 = await responseMessage3.Content.ReadAsStringAsync();
+                ViewBag.sendMessageCount = jsonData3;
                 return View(values);
             }
+           
             return View();
         }
         public async Task<IActionResult> Sendbox()
@@ -92,5 +99,6 @@ namespace HotelProject.WebUI.Controllers
             }
             return View();
         }
+   
     }
 }
